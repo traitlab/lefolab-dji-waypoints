09f6bffb28425002eec3229e47001acd7232d1bf
Vincent Le Falher <vincent.lefalher@umontreal.ca>
2024-06-27 11:03:27 -0400

    add __main__ into main.py

--- src/main.py
+++ src/main.py
@@ -8,32 +8,42 @@ from lib.create_kmz import CreateKMZ
 from lib.extract_points import ExtractPoints
 from lib.shortest_path import ShortestPath
 
-try:
-    extract_points = ExtractPoints()
-    extract_points.load()
-    extract_points.setup()
-    extract_points.get_points_elevation_from_dsm()
-    extract_points.reproject(config.from_epsg, config.to_epsg)
-    extract_points.write_global_csv()
-    extract_points.write_waypoint_csv()
 
-    shortest_path = ShortestPath()
-    shortest_path.load()
-    shortest_path.setup()
-    shortest_path.to_csv()
+def main():
 
-    build_template_kml = BuildTemplateKML()
-    build_template_kml.setup()
-    build_template_kml.generate()
-    build_template_kml.saveNewKML()
+    try:
+        extract_points = ExtractPoints()
+        extract_points.load()
+        extract_points.setup()
+        extract_points.get_points_elevation_from_dsm()
+        extract_points.reproject(config.from_epsg, config.to_epsg)
+        extract_points.write_global_csv()
+        extract_points.write_waypoint_csv()
 
-    build_waylines_wpml = BuildWaylinesWPML()
-    build_waylines_wpml.setup()
-    build_waylines_wpml.generate()
-    build_waylines_wpml.saveNewWPML()
+        shortest_path = ShortestPath()
+        shortest_path.load()
+        shortest_path.setup()
+        shortest_path.to_csv()
 
-    create_kmz = CreateKMZ()
-    create_kmz.create_kmz()
+        build_template_kml = BuildTemplateKML()
+        build_template_kml.setup()
+        build_template_kml.generate()
+        build_template_kml.saveNewKML()
 
-except ValidationError as e:
-    print(f"Configuration error: {e}")
+        build_waylines_wpml = BuildWaylinesWPML()
+        build_waylines_wpml.setup()
+        build_waylines_wpml.generate()
+        build_waylines_wpml.saveNewWPML()
+
+        create_kmz = CreateKMZ()
+        create_kmz.create_kmz()
+
+    except ValidationError as e:
+        print(f"Configuration error: {e}")
+
+        # Your code here
+        print("Running lefolab-drone")
+
+
+if __name__ == "__main__":
+    main()
