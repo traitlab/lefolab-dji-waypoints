0fddc478ab7e3eda0cc35e4cb257350afef3329e
Vincent Le Falher <vincent.lefalher@umontreal.ca>
2024-06-27 11:26:37 -0400

    - add __init__.py under lib and model folders - update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="lefolab-drone-src",
-    version="0.6.8",
+    version="0.6.9",
     author="Vincent Le Falher",
     author_email="vincent.lefalher@umontreal.ca",
     description="LEFO LAB DJI KMZ Generator",
