57cc435b2400b0d690ca6eaaa8bcb91c14b2fc98
Vincent Le Falher <vincent.lefalher@umontreal.ca>
2024-06-27 11:05:11 -0400

    - add __main__ into main.py

--- docker/docker_versions.sh
+++ docker/docker_versions.sh
@@ -2,7 +2,7 @@
 echo "# -----------------------------------------------------------------------"
 echo "script $0"
 # used to build the docker; only major.minor version. revision/buildnumber should not appear or new docker image will be build as it's named/tagged with version
-APP_VERSION=v0.6.7
+APP_VERSION=v0.6.8
 DOCKER_APP_VERSION=v0_6
 DOCKER_VOLUME_VERSION=v_drone${DOCKER_APP_VERSION}
 AUTODEPLOY=true
