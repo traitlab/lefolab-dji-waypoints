5d19894a42e190972a47d509f2ea7f596213f688
Vincent Le Falher <vincent.lefalher@umontreal.ca>
2025-04-29 18:39:41 -0400

    - reset waypointHeadingParam as before (smoothTransition) - removed hover zoom focus - use waypointSpeed 15 (vs useGlobalSpeed 1) - use waypointSpeed 1 point 3 (bfr orientedShoot)

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="lefolab-dji-waypoints",
-    version="0.2.4",
+    version="0.2.5",
     author="Vincent Le Falher",
     author_email="vincent.lefalher@umontreal.ca",
     description="LEFO LAB DJI KMZ Generator",
